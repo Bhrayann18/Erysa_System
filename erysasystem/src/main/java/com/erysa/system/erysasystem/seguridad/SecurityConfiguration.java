fc216ad4b2b1275fb493b670d1866a50fea9a35f
BrayanB <keynton888@gmail.com>
2023-08-21 11:48:42 -0500

    Fix bad repo

--- erysasystem/src/main/java/com/erysa/system/erysasystem/seguridad/SecurityConfiguration.java
+++ erysasystem/src/main/java/com/erysa/system/erysasystem/seguridad/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/form/**","/eliminar/**","/listar/**","/registro**", "/js/**", "/css/**", "/img/**")
+				.antMatchers("/form/**","/eliminar/**","/exportarExcel**/","/listar/**","/registro**", "/js/**", "/css/**", "/img/**")
 				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
 				.logout().invalidateHttpSession(true).clearAuthentication(true)
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout")
